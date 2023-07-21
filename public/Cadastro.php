54318ffc171e7169bbe9531dd45e5d9399817c3d
micael-ricardo <micael.ricardo@outlook.com>
2023-07-21 13:14:02 -0300

    Criar e usar layout separado para melhor manutenção do codigo

--- public/Cadastro.php
+++ public/Cadastro.php
@@ -1,22 +1,22 @@
-<!DOCTYPE html>
-<html>
+<?php
+ob_start();
+?>
 
-<head>
-    <title>Inserir Cliente</title>
-</head>
 <?php
 if (isset($_GET['error'])) {
     echo '<p>' . htmlspecialchars(urldecode($_GET['error'])) . '</p>';
 }
 ?>
-<body>
-    <form action="../controllers/ClienteController.php" method="post">
-        <label for="nome">Nome:</label><br>
-        <input type="text" name="nome" value="<?php echo isset($_GET['nome']) ? htmlspecialchars(urldecode($_GET['nome'])) : ''; ?>"><br>
-        <label for="cpf">CPF:</label><br>
-        <input type="text" name="cpf" value="<?php echo isset($_GET['cpf']) ? htmlspecialchars(urldecode($_GET['cpf'])) : ''; ?>"><br>
-        <input type="submit" value="Inserir">
-    </form>
-</body>
 
-</html>
+<form action="../controllers/ClienteController.php" method="post">
+    <label for="nome">Nome:</label><br>
+    <input type="text" name="nome" value="<?php echo isset($_GET['nome']) ? htmlspecialchars(urldecode($_GET['nome'])) : ''; ?>"><br>
+    <label for="cpf">CPF:</label><br>
+    <input type="text" name="cpf" value="<?php echo isset($_GET['cpf']) ? htmlspecialchars(urldecode($_GET['cpf'])) : ''; ?>"><br>
+    <input type="submit" value="Inserir">
+</form>
+
+<?php
+$conteudo = ob_get_clean();
+include 'template/layout.php';
+?>
